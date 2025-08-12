8564be875c14c0704bb05922c2c25d9295d89156
Sumit kumar <132152509+sumit8340@users.noreply.github.com>
2025-08-12 07:41:01 +0000

    Add some file

---
+++ Day-03/global_scope.py
@@ -0,0 +1,12 @@
+a = 10
+b = 8
+
+def add():
+    print(a + b)
+
+def sub():
+    print (a - b)
+
+add()
+sub()
+
---
+++ Day-03/local_scope.py
@@ -0,0 +1,13 @@
+def add():
+    a = 10
+    b = 5
+    print (a+b)
+
+def sub():
+    a = 20
+    b = 8
+    print(a-b)
+
+
+add()
+sub()
