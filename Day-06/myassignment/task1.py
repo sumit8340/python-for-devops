5bde10c9186380619cda0618989f00dd15b47791
sumit8340 <sumit8340.devops@gmail.com>
2025-08-12 20:14:49 +0530

    Update file

---
+++ Day-06/myassignment/task1.py
@@ -0,0 +1,12 @@
+a = 10
+b = 5
+
+add = a + b
+sub = a - b
+mul = a * b
+div = a / b
+
+print("Addition-->",add)
+print("Subtraction--> ",sub)
+print("Multiplication--> ",mul)
+print("Division--> ",div)
---
+++ Day-06/myassignment/task4.py
@@ -0,0 +1,7 @@
+total = 10
+total += 5
+total -= 2
+total *= 3
+total /= 3
+
+print(total)
