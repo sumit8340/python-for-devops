ccfc42fa1b03c2191daed75795215b60e01c3f52
sumit8340 <sumit8340.devops@gmail.com>
2025-08-13 00:39:50 +0530

    Loops

---
+++ Day-09/mypractice/for_loop.py
@@ -0,0 +1,19 @@
+# for_loop is using definte number of time means if we know the excate count we use for loop
+
+for i in range(10):
+    print("Monu")    # ten time Monu print
+
+#-----------------#
+
+for i in range(10):
+    print(i)          # output gives [0,1...,9]
+
+#-------------------#
+
+colors = ["Red", "Pink", "Yellow"]
+
+for x in colors:
+    print(x)   # output Red
+               #        Pink
+               #        Yellow
+               
---
+++ Day-09/mypractice/while_loop.py
@@ -0,0 +1,3 @@
+# while_loop when we are not sure about number of execution mean we don't know about how many time we execute
+
+ 
