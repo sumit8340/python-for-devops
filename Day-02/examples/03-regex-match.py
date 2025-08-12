cbb4066f8a33acbfed88ea4e9f0f523fa6f51c0a
Sumit kumar <132152509+sumit8340@users.noreply.github.com>
2025-08-12 07:41:27 +0000

    Add some file

--- Day-02/examples/03-regex-match.py
+++ Day-02/examples/03-regex-match.py
@@ -1,6 +1,6 @@
 import re
 
-text = "The quick brown fox"
+text = "The quick brown fox." # match only match when the string is started with quick
 pattern = r"quick"
 
 match = re.match(pattern, text)
---
+++ Day-02/test/uppercase.py
@@ -0,0 +1,8 @@
+text = "python is awesome"
+print(text.upper())
+print(text.lower())
+
+uppercase = text.upper()
+lowercase = text.lower()
+print("Upper_Case:", uppercase)
+print("Lower_Case:", lowercase)
