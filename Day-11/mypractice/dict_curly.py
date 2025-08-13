3416aae9ef2b4f5efb6b68121a309a34f95bb9c5
sumit8340 <sumit8340.devops@gmail.com>
2025-08-13 15:00:52 +0530

    update dict

---
+++ Day-11/mypractice/dict_curly.py
@@ -0,0 +1,8 @@
+students_properties = {
+    "Name" :"Abhi",
+    "Age" :10,
+    "Class" :11
+}
+
+
+print(students_properties)
---
+++ Day-11/mypractice/dict_curly1.py
@@ -0,0 +1,17 @@
+students_info = [
+    { "Name": "Monu",
+       "Age" : 35
+    },
+    {
+        "Name": "Sonu",
+        "Age" : 38
+
+    },
+    {
+        "Name": "Kushal",
+        "Age" : 35
+
+    }
+    
+]
+print(students_info[0]["Name"])
